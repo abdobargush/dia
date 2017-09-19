bb88149da3895238c29d9dae3fa5c86ce5180fb5
Abdulaziz Bargush <zezo.mohamed00@gmail.com>
2017-09-19 16:20:24 +0200

    Add dia_setup function.

--- functions.php
+++ functions.php
@@ -1,8 +1,42 @@
 <?php
 
-// Theme support
-add_theme_support( 'title-tag' );
-add_theme_support( 'post-thumbnails' );
+function dia_setup() {
+    /*
+     * Make theme available for translation.
+     * If you're building a theme based on DIA', use a find and replace
+     * to change 'twentyseventeen' to the name of your theme in all the template files.
+     */
+    load_theme_textdomain( 'dia' );
+	
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+	
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+	
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
+	
+	
+}
+add_action( 'after_setup_theme', 'dia_setup' );
 
 // Adding scripts and stylesheets
 function dia_scripts() {
