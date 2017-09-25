2a84dc76f7bb436c1237358697f16018c3e9b888
Abdulaziz Bargush <zezo.mohamed00@gmail.com>
2017-09-25 22:02:58 +0200

    A small php fix.

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
 			<div class="intro">
 				<h1 class="super-heading"><?php _e('404: Page not found.', 'dia') ?></h1>
 				<p><?php _e('Sorry, The page you were looking for doesn\'t exist 😕', 'dia') ?></p>
-				<p><? php _e('What about going to the <a href="..">Homepage</a>?', 'dia') ?></p>
+				<p><?php _e('What about going to the <a href="..">Homepage</a>?', 'dia') ?></p>
 			</div>
 		</div>
 	</div>
