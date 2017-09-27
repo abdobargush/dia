dba2111423a876484ac4498d8a5d316580d74d9b
Abdulaziz Bargush <zezo.mohamed00@gmail.com>
2017-09-28 00:05:05 +0200

    Make blog one column if sidebar isn't active.

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 <div class="container">
 	<div class="row">
 		<!-- Main Content -->
-		<div class="col-md-8 col-xs-12">
+			<?php if ( is_active_sidebar('blog_sidebar') ) {
+					echo ('<div class="col-md-8 col-xs-12">');
+				}
+				else {
+					echo ('<div class="col-md-8 col-xs-12 col-md-offset-2">');
+				}
+			?>
 			<h2 class="page-header"><?php wp_title(''); ?></h2>
 			<div class="blog-posts">
 				<?php get_template_part( 'content', get_post_format() ); ?>
